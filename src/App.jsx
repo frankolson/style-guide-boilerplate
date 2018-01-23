b9ffa57c3308ff1d29794af10aa64f9799570b27
Will Olson <will.olson11@gmail.com>
2018-01-23 14:24:38 -0800

    feat: First component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
 // Vendor Asssets
 import React, { Component } from 'react';
-
-// Assets
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Project Assets
+import Section from './components/Section';
 import './App.css';
-import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <Section title="Branding" />
+          </div>
+
+          <div className="col-md-6">
+            <Section title="Colors" />
+          </div>
+
+          <div className="col-md-6">
+            <Section title="Typography" />
+          </div>
+
+          <div className="col-md-6">
+            <Section title="Buttons" />
+          </div>
+
+          <div className="col-md-6">
+            <Section title="Icons" />
+          </div>
+
+          <div className="col-md-6">
+            <Section title="Form" />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Section.jsx
@@ -0,0 +1,12 @@
+// Vendor Asssets
+import React from 'react';
+
+const Section = ({ title }) => (
+  <div className="card">
+    <div className="card-body">
+      {title}
+    </div>
+  </div>
+);
+
+export default Section;
