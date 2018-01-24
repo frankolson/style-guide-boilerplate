6db09df2beb66777e0dc43d13468183610c1d17f
Will Olson <will.olson11@gmail.com>
2018-01-23 21:11:35 -0800

    feat: Add PropTypes

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,5 +1,16 @@
 // Vendor Asssets
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  children: PropTypes.node,
+  position: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+const defaultProps = {
+  children: null,
+};
 
 const Section = ({ children, position, title }) => (
   <div>
@@ -12,5 +23,8 @@ const Section = ({ children, position, title }) => (
     {children}
   </div>
 );
+
+Section.propTypes = propTypes;
+Section.defaultProps = defaultProps;
 
 export default Section;
